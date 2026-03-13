b0c143697de0a63f7b276040336eee94b52340e5
JoseMayson <limamarquesr@gmail.com>
2026-03-13 13:45:36 -0300

    Exercício de revisao

--- ed/Revisâo/casais/go/draft.go
+++ ed/Revisâo/casais/go/draft.go
@@ -7,21 +7,17 @@ func main() {
 	fmt.Scan(&n)
 
 	animal := make([]int, n)
-	descasados := make([]int, n)
 
 	for i := 0; i < n; i++ {
 		fmt.Scan(&animal[i])
-		if animal[i] < 0 {
-			descasados[i] = animal[i]
-		}
 	}
 
 	var t int
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			if animal[i] > 0 {
-				if animal[i] == (descasados[j] * (-1)) {
-					descasados[j] = 0
+				if animal[i] == (animal[j] * (-1)) {
+					animal[j] = 0
 					t++
 				}
 			}
