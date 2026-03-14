509f09ef6dd9a8a0471ecde526cb3519ed8f60bd
JoseMayson <limamarquesr@gmail.com>
2026-03-14 13:01:10 -0300

    Exercicio basico de fup

---
+++ fup/Operações basic/media/go/draft.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main() {
+    var a , b float64
+    fmt.Scan(&a, &b)
+
+    fmt.Printf("%.1f\n", (a + b)/ 2)
+}
---
+++ fup/Operações basic/resto/go/draft.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+    var a,b int
+    fmt.Scan(&a, &b)
+    fmt.Printf("%d %d\n", a / b, a % b)
+}
