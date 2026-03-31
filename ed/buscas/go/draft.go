a209bf255d821581573ccb71b541e571228af80e
JoseMayson <limamarquesr@gmail.com>
2026-03-31 00:53:03 -0300

    atualização

--- ed/buscas/go/draft.go
+++ ed/buscas/go/draft.go
@@ -3,19 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	var qtdentrada int
-	fmt.Scan(&qtdentrada)
+	var n int
+	fmt.Scan(&n)
 
-	entrada := make([]string, qtdentrada)
-	for i := range qtdentrada {
+	entrada := make([]string, n)
+
+	for i := range entrada {
 		fmt.Scan(&entrada[i])
 	}
 
-	var qtdconsulta int
-	fmt.Scan(&qtdconsulta)
-	consulta := make([]string, qtdconsulta)
-	for i := range qtdconsulta {
+	var x int
+	fmt.Scan(&x)
+
+	consulta := make([]string, x)
+
+	for i := range consulta {
 		fmt.Scan(&consulta[i])
 	}
 
+	temp := make(map[string]int)
+
+	for _, p := range entrada {
+		temp[p]++
+	}
+
+	for i := range consulta {
+		fmt.Printf("%d", temp[consulta[i]])
+		if i < len(consulta)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Println()
 }
