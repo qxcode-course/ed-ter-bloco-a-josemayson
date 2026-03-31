485e2acf8bdd3286e1ea4abe613f04285b7ddeac
JoseMayson <limamarquesr@gmail.com>
2026-03-31 08:58:49 -0300

    nova questão

--- ed/rotacoes/go/draft.go
+++ ed/rotacoes/go/draft.go
@@ -1,5 +1,26 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("Hello, World!")
+	var t, r int
+	fmt.Scan(&t, &r)
+
+	var lista []int
+
+	for i := 1; i <= t; i++ {
+		lista = append(lista, i)
+	}
+
+	for i := 0; i < r; i++ {
+		aux := lista[len(lista)-1]
+		copy(lista[1:], lista[:len(lista)-1])
+		lista[0] = aux
+	}
+	fmt.Print("[ ")
+	for i := range lista {
+		fmt.Printf("%d ", lista[i])
+	}
+	fmt.Print("]\n")
+
 }
